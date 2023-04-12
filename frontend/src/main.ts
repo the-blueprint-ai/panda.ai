b17cfc4febf5b72271fc398e29f81317416d6828
seanbetts <seanbetts@icloud.com>
2023-04-12 22:26:51 +0100

    Google Analytics Added

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueGtag from "vue-gtag";
 import SuperTokens from "supertokens-web-js";
 import "./assets/styles/panda-main.css";
 import { SuperTokensWebJSConfig } from "./config";
@@ -16,5 +17,14 @@ axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
 
 app.use(router);
 app.use(store);
+app.use(
+  VueGtag,
+  {
+    appName: 'panda.ai',
+    pageTrackerScreenviewEnabled: true,
+    config: { id: "G-HS1DM7DQXW" },
+  },
+  router
+);
 
 app.mount("#app");

bd504579cb7a26df9413b7a2b5d4cb873c8f59ce
seanbetts <seanbetts@icloud.com>
2023-04-24 20:41:32 +0100

    Email updates

---
+++ frontend/src/views/UnsubscribeView.vue
@@ -0,0 +1,24 @@
+<script>
+import { defineComponent } from "vue";
+import navBar from "../components/navBar.vue";
+import navFooter from "../components/navFooter.vue";
+
+export default defineComponent({
+  components: {
+    navBar,
+    navFooter,
+  },
+});
+</script>
+
+<template>
+  <main>
+    <navBar></navBar>
+    <div class="body">
+      <div>
+        <h1>UNSUBSCRIBE VIEW</h1>
+      </div>
+    </div>
+    <navFooter></navFooter>
+  </main>
+</template>
